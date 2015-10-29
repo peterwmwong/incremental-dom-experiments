d2e423b2a3933a7005a1dd9e623e4c0415834547
peterwmwong <peter.wm.wong@gmail.com>
2015-10-29 17:54:09 -0500

    Small tweaks

--- src/components/AppDrawer.jsx
+++ src/components/AppDrawer.jsx
@@ -8,13 +8,13 @@ export default ({user, enabled, onSelectSource, onRequestDisable})=>
     <div className='AppDrawer-sheet' onclick={onRequestDisable} />
     <div className='AppDrawer-content'>
       {user &&
-        <div className="List-item layout horizontal center">
-          <Avatar avatarUrl="" className="l-margin-r2" />
+        <div className="List-item layout horizontal center l-padding-v6">
+          <Avatar avatarUrl="" className="l-margin-r4" />
           {user.username}
         </div>
       }
       {user && user.sources.map(source=>
-        <div key={source.displayName} className="List-item" onclick={()=>onSelectSource(source)} >
+        <div key={source.displayName} className="List-item" onclick={()=>onSelectSource(source)}>
           <SourceName displayName={source.displayName}/>
         </div>
       )}
